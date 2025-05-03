d3b6717fcc7cacf2a1eca3eae893b1908ced7576
Hussein <143280315+46-HA@users.noreply.github.com>
2025-05-02 23:20:10 -0500

    getting started w/ tailwind

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # afterglow
 Afterglow is an app that revolves around building good habits and being more productive.
+
+# Getting Started
---
+++ afterglow/src/App.jsx
@@ -0,0 +1,31 @@
+import './index.css';
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white font-sans">
+      <section className="flex flex-col items-center justift-center h-screen text center"></section>
+      <h1 className="text-5xl font-bold text-center mb-4 tacking-wide">afterglow.</h1>
+      <p className="text-xl text-gray-400 max-w-xl">
+        Track your goals
+        </p>
+        <button className="mt-6 px-6 py-2 bg-white text-black font-semibold rounded hover:bg-gray-200 transition">
+        Get Started
+        </button>
+    </div>
+  )
+}
+/*
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>afterglow.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
+*/ 
